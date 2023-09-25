41436272c9dae3d57402fc0600279b75c1831f13
FlorianGrollich <F-Grollich@web.de>
2023-09-25 11:32:44 +0200

    feat: setup tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
 
     return (
         <div className="App">
-            <nav className="bg-blue-500 p-4 fixed top-0 w-full ">
+            <nav className="bg-blue-400 p-4 fixed top-0 w-full">
                     <div className="justify-between text-">
                         <a href="#" className="text-white mx-2">Home</a>
                         <a href="#" className="text-white mx-2">About</a>
