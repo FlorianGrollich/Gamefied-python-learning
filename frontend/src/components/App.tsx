3c5d99328897cc0bf24e564f02f41d8c7a5584aa
FlorianGrollich <F-Grollich@web.de>
2023-10-02 13:24:13 +0200

    add logo.svg

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import logo from '../logo.svg';
 function App() {
     return (
-        <div className="App">
-            <nav className="bg-blue-400 p-4 fixed top-0 w-full">
-                    <div className="justify-between text-">
-                        <a href="#" className="text-white mx-2">Home</a>
-                        <a href="#" className="text-white mx-2">About</a>
+        <div className="App bg-slate-950">
+            <nav className="bg-slate-800 p-4 fixed top-0 w-full">
+                    <div className="justify-between text-black">
+                        <a href="#" className="mx-2">Test</a>
+                        <a href="#" className="text-white mx-2">Test</a>
                         <a href="#" className="text-white mx-2">Contact</a>
                 </div>
             </nav>
