aca3af16d2e6e183022a66c5e695093ec2004c30
beneloe <benedikt.loe@hotmail.com>
2023-10-14 16:28:42 +0200

    chore: add server process cleanup protocol to scripts

--- scripts/app-startup-check.sh
+++ scripts/app-startup-check.sh
@@ -3,14 +3,18 @@
 echo "Starting the application..."
 npm start &
 
+APP_PID=$!
+echo "Application started with PID: $APP_PID"
+
 sleep 10
 
 curl -fs http://localhost:3000 > /dev/null 
 
 if [ $? -ne 0 ]; then
     echo "Error: Application failed to start"
+    kill $APP_PID
     exit 2
 fi
 
 echo "Application is running and endpoint reachable!"
-exit 0
+wait $APP_PID
--- scripts/full-check.sh
+++ scripts/full-check.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+cleanup() {
+    echo "Caught Signal ... shutting down."
+    if [ ! -z "$SERVER_PID" ]; then
+        if kill -0 $SERVER_PID 2>/dev/null; then
+            echo "Killing server process with PID $SERVER_PID"
+            kill $SERVER_PID
+        else
+            echo "No process found with PID $SERVER_PID"
+        fi
+    fi
+    exit 1
+}
+
+trap cleanup SIGINT SIGTERM
+
 ./dependency-check.sh
-
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-./app-startup-check.sh
+./app-startup-check.sh &
 
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+SERVER_PID=$!
 
-echo "All checks passed!"
+echo "All checks passed! Server is running in the background."
+
+wait
