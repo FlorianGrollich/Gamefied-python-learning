339bb103a4abdda51c793a8a9a04c4aee7972c0a
FlorianGrollich <F-Grollich@web.de>
2023-10-15 15:47:45 +0200

    fix: start script

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,49 @@
 import express from 'express';
-import { Server as WebSocketServer } from 'ws';
-import { createServer } from 'http';
+import {Server as WebSocketServer} from 'ws';
+import {createServer} from 'http';
+import {DataSource} from 'typeorm';
 
 const port = 3200;
+
 const app = express();
 
 const server = createServer(app);
 
-const wss = new WebSocketServer({ noServer: true });
+const wss = new WebSocketServer({noServer: true});
 
+
+const PostgresDataSource = new DataSource({
+    type: "postgres",
+    host: "db",
+    port: 5432,
+    username: "user",
+    password: "password",
+    database: "mydatabase",
+    synchronize: true,
+    logging: false,
+    entities: [
+        "src/entity/**/*.ts"
+    ],
+});
+
+PostgresDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+    });
 wss.on('connection', (ws) => {
     console.log('Client connected');
-    
+
     ws.on('message', (message) => {
         console.log(`Received: ${message}`);
     });
-    
+
     ws.on('close', () => {
         console.log('Client disconnected');
     });
-    
+
     ws.send('Welcome to the WebSocket server!');
 });
 
