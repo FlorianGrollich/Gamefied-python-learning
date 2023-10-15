03ddc43819b7c87a456bbef995c0e5b936acae78
beneloe <benedikt.loe@hotmail.com>
2023-10-15 19:34:30 +0200

    fix: move routing into App.tsx

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import logo from '../logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RegistrationPage from './RegistrationPage';
+
 function App() {
     return (
         <div className="App bg-slate-950">
             <nav className="bg-slate-800 p-4 fixed top-0 w-full">
-                    <div className="justify-between text-black">
-                        <a href="#" className="mx-2">Test</a>
-                        <a href="#" className="text-white mx-2">Test</a>
-                        <a href="#" className="text-white mx-2">Contact</a>
+                <div className="justify-between text-black">
+                    <a href="#" className="mx-2">Test</a>
+                    <a href="#" className="text-white mx-2">Test</a>
+                    <a href="#" className="text-white mx-2">Contact</a>
                 </div>
             </nav>
+            <Router>
+                <Routes>
+                    <Route path="/register" element={<RegistrationPage />} />
+                    <Route path="/" element={<h1>Welcome to the Home Page</h1>} />
+                </Routes>
+            </Router>
         </div>
     );
 }
