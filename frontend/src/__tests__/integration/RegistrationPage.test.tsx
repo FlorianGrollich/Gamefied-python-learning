37321a65b1e82524a011e126b3ec74516dd4471e
beneloe <benedikt.loe@hotmail.com>
2023-10-16 11:11:08 +0200

    test: add RegistrationPage test

---
+++ frontend/src/__tests__/integration/RegistrationPage.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage';
+
+describe('RegistrationPage Component', () => {
+  test('renders RegistrationPage component', () => {
+    render(<RegistrationPage />);
+    const registrationHeading = screen.getByRole('heading', { name: /register/i });
+    expect(registrationHeading).toBeInTheDocument();
+  });
+});
