ee060ef16f6ec5c65fba8e8d4b27b8409f9fe3b8
beneloe <benedikt.loe@hotmail.com>
2023-10-17 00:41:20 +0200

    test: add RegistrationForm API mock test

--- frontend/src/components/RegistrationForm/RegistrationForm.tsx
+++ frontend/src/components/RegistrationForm/RegistrationForm.tsx
@@ -6,6 +6,7 @@ const RegistrationForm: React.FC = () => {
     password: '',
     passwordConfirmation: '',
   });
+  const [registrationStatus, setRegistrationStatus] = useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -18,38 +19,46 @@ const RegistrationForm: React.FC = () => {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     // Form submission logic
+    if (formData.password === formData.passwordConfirmation) {
+      setRegistrationStatus('Registration successful');
+    } else {
+      setRegistrationStatus('Passwords do not match');
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="p-4 space-y-4">
-      <input 
-        type="email" 
-        name="email" 
-        placeholder="Email" 
-        value={formData.email} 
-        onChange={handleChange} 
-        className="p-2 m-0.5 border rounded"
-      />
-      <input 
-        type="password" 
-        name="password" 
-        placeholder="Password" 
-        value={formData.password} 
-        onChange={handleChange} 
-        className="p-2 m-0.5 border rounded"
-      />
-      <input 
-        type="password" 
-        name="passwordConfirmation" 
-        placeholder="Confirm Password" 
-        value={formData.passwordConfirmation} 
-        onChange={handleChange} 
-        className="p-2 m-0.5 border rounded"
-      />
-      <button type="submit" className="p-2 m-0.5 bg-blue-500 text-white rounded">
-        Sign Up
-      </button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit} className="p-4 space-y-4">
+        <input 
+          type="email" 
+          name="email" 
+          placeholder="Email" 
+          value={formData.email} 
+          onChange={handleChange} 
+          className="p-2 m-0.5 border rounded"
+        />
+        <input 
+          type="password" 
+          name="password" 
+          placeholder="Password" 
+          value={formData.password} 
+          onChange={handleChange} 
+          className="p-2 m-0.5 border rounded"
+        />
+        <input 
+          type="password" 
+          name="passwordConfirmation" 
+          placeholder="Confirm Password" 
+          value={formData.passwordConfirmation} 
+          onChange={handleChange} 
+          className="p-2 m-0.5 border rounded"
+        />
+        <button type="submit" className="p-2 m-0.5 bg-blue-500 text-white rounded">
+          Sign Up
+        </button>
+      </form>
+      {registrationStatus && <p className="mt-4 text-center">{registrationStatus}</p>}
+    </div>
   );
 };
 
