5ff9590f363c654cc8b5d84fee327929f2518ed5
beneloe <benedikt.loe@hotmail.com>
2023-10-25 20:06:26 +0200

    chore: update routes to include login endpoint

--- server/routes.ts
+++ server/routes.ts
@@ -1,4 +1,5 @@
 import { UserController } from "./controller/UserController"
+import { AuthController } from "./controller/AuthController"
 
 export const Routes = [{
     method: "get",
@@ -20,4 +21,9 @@ export const Routes = [{
     route: "/users/:id",
     controller: UserController,
     action: "remove"
+}, {
+    method: "post",
+    route: "/api/login",
+    controller: AuthController,
+    action: "login"
 }]
