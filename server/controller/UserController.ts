e51e455227508b70297834050bac2c58c2789266
beneloe <benedikt.loe@hotmail.com>
2023-10-25 19:29:57 +0200

    refactor: add bcrypt hashing to User entity and improve UserController logic

--- server/controller/UserController.ts
+++ server/controller/UserController.ts
@@ -1,53 +1,53 @@
-import { NextFunction, Request, Response } from "express"
-import {PostgresDataSource} from "../utils/data-source";
-import {User} from "../entity/User";
+import { NextFunction, Request, Response } from "express";
+import { PostgresDataSource } from "../utils/data-source";
+import { User } from "../entity/User";
 
 export class UserController {
 
-    private userRepository = PostgresDataSource.getRepository(User)
+    private userRepository = PostgresDataSource.getRepository(User);
 
     async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find()
+        return this.userRepository.find();
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
-        const id = parseInt(request.params.id)
+        const id = parseInt(request.params.id);
 
-
-        const user = await this.userRepository.findOne({
-            where: { id }
-        })
+        const user = await this.userRepository.findOne({ where: { id: id } });
 
         if (!user) {
             return response.status(404).json({ error: "Unregistered user" });
         }
-        return user
+        return user;
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
-        const { firstName, lastName, age } = request.body;
+        const { username, email, password } = request.body;
 
-        const user = Object.assign(new User(), {
-            firstName,
-            lastName,
-            age
-        })
+        const user = new User();
+        user.username = username;
+        user.email = email;
+        user.setPassword(password);
 
-        return this.userRepository.save(user)
+        try {
+            await this.userRepository.save(user);
+            return response.status(201).json({ message: "User created successfully" });
+        } catch (error) {
+            console.error(error);
+            return response.status(500).json({ error: "Internal Server Error" });
+        }
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
-        const id = parseInt(request.params.id)
+        const id = parseInt(request.params.id);
 
-        let userToRemove = await this.userRepository.findOneBy({ id })
+        let userToRemove = await this.userRepository.findOne({ where: { id: id } });
 
         if (!userToRemove) {
-            return "this user not exist"
+            return response.status(404).json({ error: "User not found" });
         }
 
-        await this.userRepository.remove(userToRemove)
-
-        return "user has been removed"
+        await this.userRepository.remove(userToRemove);
+        return response.status(200).json({ message: "User has been removed" });
     }
-
 }
--- server/entity/User.ts
+++ server/entity/User.ts
@@ -1,5 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column} from "typeorm"
-
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate } from "typeorm";
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 export class User {
@@ -11,4 +11,27 @@ export class User {
 
     @Column()
     email: string;
+
+    @Column()
+    hashedPassword: string;
+
+    private plainPassword?: string;
+
+    setPassword(password: string) {
+        this.plainPassword = password;
+    }
+
+    async validatePassword(password: string): Promise<boolean> {
+        return bcrypt.compare(password, this.hashedPassword);
+    }
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    private async hashPassword() {
+        if (this.plainPassword) {
+            const salt = await bcrypt.genSalt(12);
+            this.hashedPassword = await bcrypt.hash(this.plainPassword, salt);
+            this.plainPassword = undefined;
+        }
+    }
 }
