623f230b4f5d10df51f489f2d9cb2c3e54922054
beneloe <benedikt.loe@hotmail.com>
2023-10-25 20:14:10 +0200

    feat: update LoginForm to send request to backend

--- frontend/src/LoginForm/LoginForm.tsx
+++ frontend/src/LoginForm/LoginForm.tsx
@@ -1,36 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const LoginForm: React.FC = () => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
-  })
-  const [loginStatus, setLoginStatus] = useState('')
+  });
+  const [loginStatus, setLoginStatus] = useState<string | null>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
-    })
-  }
+    });
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    const response = await fetch('/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-    const data = await response.json()
-    if (data.token) {
-      setLoginStatus('Login successful')
-    } else {
-      setLoginStatus('Login failed')
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://backend-url/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.status === 200) {
+        setLoginStatus('Login successful');
+      } else if (response.status === 401) {
+        setLoginStatus('Invalid email or password');
+      } else if (response.status === 404) {
+        setLoginStatus('Endpoint not found');
+      } else {
+        setLoginStatus('An error occurred. Please try again later.');
+      }
+    } catch (error) {
+      console.error('Login error:', error);
+      setLoginStatus('An error occurred. Please try again later.');
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className="p-4 space-y-4">
@@ -52,13 +62,15 @@ const LoginForm: React.FC = () => {
       />
       <button
         type="submit"
-        className="p-2 m-0.5 bg-blue-500 text-white rounded"
+        className="p-2 m-0.5 bg-blue-500 text-white rounded hover:bg-blue-600"
       >
         Login
       </button>
-      {loginStatus && <p>{loginStatus}</p>}
+      {loginStatus && (
+        <p className="text-red-500">{loginStatus}</p>
+      )}
     </form>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
