cd342d9ca1b7b39da2de9e2c87babff354bdf3e6
beneloe <benedikt.loe@hotmail.com>
2023-10-26 18:06:56 +0200

    refactor: simplify the login method in AuthController.ts by removing the try-catch block

--- server/controller/AuthController.ts
+++ server/controller/AuthController.ts
@@ -12,29 +12,24 @@ export class AuthController {
     private userRepository = PostgresDataSource.getRepository(User);
 
     async login(request: Request, response: Response, next: NextFunction) {
-        try {
-            const { email, password } = request.body;
+        const { email, password } = request.body;
 
-            const user = await this.userRepository.findOne({ where: { email } });
-            if (!user) {
-                return response.status(401).json({ error: "Invalid email or password" });
-            }
-
-            const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
-            if (!isPasswordValid) {
-                return response.status(401).json({ error: "Invalid email or password" });
-            }
-
-            const secret = process.env.JWT_SECRET;
-            if (!secret) {
-                throw new Error('JWT_SECRET is not defined in .env file');
-            }
-
-            const token = jwt.sign({ userId: user.id }, secret, { expiresIn: '1h' });
-            return response.json({ token });
-        } catch (error) {
-            console.error(error);
-            return response.status(500).json({ error: "Internal Server Error" });
+        const user = await this.userRepository.findOne({ where: { email } });
+        if (!user) {
+            return response.status(401).json({ error: "Invalid email or password" });
         }
+
+        const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
+        if (!isPasswordValid) {
+            return response.status(401).json({ error: "Invalid email or password" });
+        }
+
+        const secret = process.env.JWT_SECRET;
+        if (!secret) {
+            throw new Error('JWT_SECRET is not defined in .env file');
+        }
+
+        const token = jwt.sign({ userId: user.id }, secret, { expiresIn: '1h' });
+        return response.json({ token });
     }
 }
