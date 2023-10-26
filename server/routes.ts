aed7e05cd094ac7fc8c6db567fd792bef52c9715
beneloe <benedikt.loe@hotmail.com>
2023-10-26 18:10:40 +0200

    refactor: routes in server/routes.ts to handle responses directly within the route definition

--- server/routes.ts
+++ server/routes.ts
@@ -1,29 +1,65 @@
-import { UserController } from "./controller/UserController"
-import { AuthController } from "./controller/AuthController"
+import { Request, Response, NextFunction } from 'express';
+import { UserController } from "./controller/UserController";
+import { AuthController } from "./controller/AuthController";
 
 export const Routes = [{
     method: "get",
     route: "/users",
     controller: UserController,
-    action: "all"
+    action: (req: Request, res: Response <any>, next: NextFunction) => {
+        const result = new UserController().all(req, res, next);
+        if (result instanceof Promise) {
+            result.then(data => data !== null && data !== undefined ? res.send(data) : undefined);
+        } else if (result !== null && result !== undefined) {
+            res.json(result);
+        }
+    }
 }, {
     method: "get",
     route: "/users/:id",
     controller: UserController,
-    action: "one"
+    action: (req: Request, res: Response <any>, next: NextFunction) => {
+        const result = new UserController().one(req, res, next);
+        if (result instanceof Promise) {
+            result.then(data => data !== null && data !== undefined ? res.send(data) : undefined);
+        } else if (result !== null && result !== undefined) {
+            res.json(result);
+        }
+    }
 }, {
     method: "post",
     route: "/api/register",
     controller: UserController,
-    action: "register"
+    action: (req: Request, res: Response <any>, next: NextFunction) => {
+        const result = new UserController().register(req, res, next);
+        if (result instanceof Promise) {
+            result.then(data => data !== null && data !== undefined ? res.send(data) : undefined);
+        } else if (result !== null && result !== undefined) {
+            res.json(result);
+        }
+    }
 }, {
     method: "delete",
     route: "/users/:id",
     controller: UserController,
-    action: "remove"
+    action: (req: Request, res: Response <any>, next: NextFunction) => {
+        const result = new UserController().remove(req, res, next);
+        if (result instanceof Promise) {
+            result.then(data => data !== null && data !== undefined ? res.send(data) : undefined);
+        } else if (result !== null && result !== undefined) {
+            res.json(result);
+        }
+    }
 }, {
     method: "post",
     route: "/api/login",
     controller: AuthController,
-    action: "login"
-}]
+    action: (req: Request, res: Response <any>, next: NextFunction) => {
+        const result = new AuthController().login(req, res, next);
+        if (result instanceof Promise) {
+            result.then(data => data !== null && data !== undefined ? res.send(data) : undefined);
+        } else if (result !== null && result !== undefined) {
+            res.json(result);
+        }
+    }
+}];
