4f735be254619bb3afe280adbceb2802b184b9ca
beneloe <benedikt.loe@hotmail.com>
2023-11-02 18:24:51 +0100

    test: refactor LoginPage tests for better readability and maintainability

--- frontend/src/__tests__/integration/LoginPage.test.tsx
+++ frontend/src/__tests__/integration/LoginPage.test.tsx
@@ -1,13 +1,24 @@
-import '@testing-library/jest-dom'
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import LoginPage from '../../LoginPage/LoginPage'
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import LoginPage from '../../LoginPage/LoginPage';
+
+const LOGIN_BUTTON_TEXT = /login/i;
 
 describe('LoginPage', () => {
-  it('renders correctly', () => {
-    const mockOnLogin = jest.fn()
-    render(<LoginPage onLogin={mockOnLogin} />)
-    const loginButton = screen.getByRole('button', { name: /login/i })
-    expect(loginButton).toBeInTheDocument()
-  })
-})
+  let mockOnLogin;
+
+  const setupLoginPage = () => {
+    mockOnLogin = jest.fn();
+    render(<LoginPage onLogin={mockOnLogin} />);
+  };
+
+  beforeEach(() => {
+    setupLoginPage();
+  });
+
+  it('renders the login button correctly', () => {
+    const loginButton = screen.getByRole('button', { name: LOGIN_BUTTON_TEXT });
+    expect(loginButton).toBeInTheDocument();
+  });
+});
