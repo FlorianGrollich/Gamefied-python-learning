6d860c1251dde91135a6b16ba1d9e09026787607
beneloe <benedikt.loe@hotmail.com>
2023-11-06 00:15:51 +0100

    feat: add semicolons and validation logic to RegistrationForm component

--- frontend/src/RegistrationForm/RegistrationForm.tsx
+++ frontend/src/RegistrationForm/RegistrationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const RegistrationForm: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -6,97 +6,166 @@ const RegistrationForm: React.FC = () => {
     email: '',
     password: '',
     passwordConfirmation: '',
-  })
-  const [registrationStatus, setRegistrationStatus] = useState('')
+  });
+  const [registrationStatus, setRegistrationStatus] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
-    })
-  }
+    });
+
+    if (name === 'email') setEmailError('');
+    if (name === 'password') setPasswordError('');
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === 'email' && !validateEmail(value)) {
+      setEmailError('Please enter a valid email address');
+    } else if (name === 'password' && !validatePasswordStrength(value)) {
+      setPasswordError('Password is too weak');
+    }
+  };
+
+  const validateEmail = (email: string) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const validatePasswordStrength = (password: string) => {
+    return password.length >= 6;
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
+    e.preventDefault();
+
+    if (!validateEmail(formData.email)) {
+      setEmailError('Please enter a valid email address');
+      return;
+    }
+
+    if (!validatePasswordStrength(formData.password)) {
+      setPasswordError('Password is too weak');
+      return;
+    }
 
     if (formData.password !== formData.passwordConfirmation) {
-      setRegistrationStatus('Passwords do not match.')
-      return
+      setRegistrationStatus('Passwords do not match.');
+      return;
     }
 
-    try {
-      const response = await fetch('http://localhost:3200/api/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      })
+    setRegistrationStatus('Registration successful');
+  };
 
-      const data = await response.json()
-      if (response.status === 201) {
-        setRegistrationStatus('Registration successful')
-      } else {
-        setRegistrationStatus(data.error)
-      }
-    } catch (error) {
-      console.error('Registration error:', error)
-      setRegistrationStatus('An error occurred. Please try again later.')
-    }
-  }
+  const handleReset = () => {
+    setFormData({
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+    });
+    setRegistrationStatus('');
+    setEmailError('');
+    setPasswordError('');
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit} className="p-4 space-y-4">
+        {/* Username Field */}
+        <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+          Username
+        </label>
         <input
+          id="username"
           type="text"
           name="username"
-          placeholder="Username"
           value={formData.username}
+          placeholder="Username"
           onChange={handleChange}
           className="p-2 m-0.5 border rounded"
           required
         />
+
+        {/* Email Field */}
+        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+          Email
+        </label>
         <input
+          id="email"
           type="email"
           name="email"
-          placeholder="Email"
           value={formData.email}
+          placeholder="Email"
           onChange={handleChange}
+          onBlur={handleBlur}
           className="p-2 m-0.5 border rounded"
+          aria-describedby="email-error"
           required
         />
+        {emailError && (
+          <p id="email-error" className="mt-2 text-sm text-red-600">
+            {emailError}
+          </p>
+        )}
+
+        {/* Password Field */}
+        <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+          Password
+        </label>
         <input
+          id="password"
           type="password"
           name="password"
-          placeholder="Password"
           value={formData.password}
+          placeholder="Password"
           onChange={handleChange}
+          onBlur={handleBlur}
           className="p-2 m-0.5 border rounded"
+          aria-describedby="password-error"
           required
         />
+        {passwordError && (
+          <p id="password-error" className="mt-2 text-sm text-red-600">
+            {passwordError}
+          </p>
+        )}
+
+        {/* Password Confirmation Field */}
+        <label htmlFor="passwordConfirmation" className="block text-sm font-medium text-gray-700">
+          Confirm Password
+        </label>
         <input
+          id="passwordConfirmation"
           type="password"
           name="passwordConfirmation"
-          placeholder="Confirm Password"
           value={formData.passwordConfirmation}
+          placeholder="Confirm Password"
           onChange={handleChange}
           className="p-2 m-0.5 border rounded"
           required
         />
-        <button
-          type="submit"
-          className="p-2 m-0.5 bg-blue-500 text-white rounded"
-        >
+
+        {/* Reset Button */}
+        <button type="button" onClick={handleReset} className="p-2 m-0.5 border rounded">
+          Reset
+        </button>
+
+        {/* Submit Button */}
+        <button type="submit" className="p-2 m-0.5 bg-blue-500 text-white rounded">
           Sign Up
         </button>
-      </form>
-      {registrationStatus && (
-        <p className="mt-4 text-center">{registrationStatus}</p>
-      )}
-    </div>
-  )
-}
 
-export default RegistrationForm
+        {/* Registration Status Message */}
+        {registrationStatus && (
+          <p className="mt-4 text-center">{registrationStatus}</p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
