33a8f12361040e3f71f3b10c13c2208aa9a475ca
beneloe <benedikt.loe@hotmail.com>
2023-11-06 21:25:52 +0100

    style: delete heading in login and registration pages

--- frontend/src/RegistrationPage/RegistrationPage.tsx
+++ frontend/src/RegistrationPage/RegistrationPage.tsx
@@ -3,8 +3,7 @@ import RegistrationForm from '../RegistrationForm/RegistrationForm'
 
 const RegistrationPage: React.FC = () => {
   return (
-    <div>
-      <h2 className="text-white text-2xl mt-14 mb-4 p-5">Register</h2>
+    <div className='mt-8'>
       <RegistrationForm />
     </div>
   )
