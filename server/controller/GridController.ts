973a72f995dcfce0ae044d3ea0fd60ca6bd6659d
FlorianGrollich <F-Grollich@web.de>
2023-11-07 19:08:26 +0100

    feat: add createNewGrid

--- server/controller/GridController.ts
+++ server/controller/GridController.ts
@@ -1,6 +1,6 @@
 import {Grid} from "../entity/Grid";
 import {PostgresDataSource} from "../utils/data-source";
-import {NextFunction} from "express";
+import {Response, Request, NextFunction} from "express";
 
 
 export class GridController {
@@ -8,11 +8,25 @@ export class GridController {
     private gridRepository = PostgresDataSource.getRepository(Grid);
 
     constructor() {
+        this.createNewGrid = this.createNewGrid.bind(this);
+        this.initialGrid = this.initialGrid.bind(this);
+    }
+
+    async createNewGrid(request: Request, response: Response, next: NextFunction) {
+        const {grid} = request.body;
+        const newGrid = new Grid();
+        newGrid.grid = grid;
+        await this.gridRepository.save(newGrid).catch((err) => {
+            return response.status(400).json({error: err});
+        });
+
+        return response.status(200).json({message: "Grid created successfully"});
+
     }
 
     async initialGrid(request: Request, response: Response, next: NextFunction) {
         const grid = await this.gridRepository.findOneBy({id: 1});
-        return grid
+        return response.status(200).json(grid);
     }
 
 
