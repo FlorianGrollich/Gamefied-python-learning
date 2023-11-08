e6ec4a82e146d70bc29db51d325fb505765ed0c8
beneloe <benedikt.loe@hotmail.com>
2023-11-09 00:25:16 +0100

    chore: run lint-format-all after merge

--- server/utils/data-source.ts
+++ server/utils/data-source.ts
@@ -1,17 +1,16 @@
-import {DataSource} from "typeorm";
-import {BananaEntities} from "../entity/_BananaEntities";
-
+import { DataSource } from 'typeorm'
+import { BananaEntities } from '../entity/_BananaEntities'
 
 export const PostgresDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USERNAME || "user",
-    password: process.env.DB_PASSWORD || "password",
-    database: process.env.DB_NAME || "mydatabase",
-    synchronize: true,
-    logging: false,
-    entities: BananaEntities,
-    migrations: [],
-    subscribers: [],
-});
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'user',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'mydatabase',
+  synchronize: true,
+  logging: false,
+  entities: BananaEntities,
+  migrations: [],
+  subscribers: [],
+})
