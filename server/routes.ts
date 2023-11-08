78e22455931b42ab01eb85c51dce95d268cf2667
FlorianGrollich <F-Grollich@web.de>
2023-11-08 21:47:50 +0100

    feat: add GridController routes

--- server/routes.ts
+++ server/routes.ts
@@ -1,5 +1,6 @@
-import { UserController } from "./controller/UserController";
-import { AuthController } from "./controller/AuthController";
+import {UserController} from "./controller/UserController";
+import {AuthController} from "./controller/AuthController";
+import {GridController} from "./controller/GridController";
 
 export const Routes = [{
     method: "get",
@@ -26,4 +27,17 @@ export const Routes = [{
     route: "/api/login",
     controller: AuthController,
     action: "login"
-}];
+}, {
+    method: "get",
+    route: "/api/grid/initial",
+    controller: GridController,
+    action: "initialGrid"
+}, {
+    method: "post",
+    route: "/api/grid/create",
+    controller: GridController,
+    action: "createNewGrid"
+}
+
+
+];
