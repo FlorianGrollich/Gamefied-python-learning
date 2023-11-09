301156f84623bdd912e68156ad8651c3fe446ceb
beneloe <benedikt.loe@hotmail.com>
2023-11-09 23:14:57 +0100

    chore: add NavBar, types.d.ts, token.ts in merge

--- frontend/src/NavBar/NavBar.tsx
+++ frontend/src/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { User } from 'src/types/types';
+import { User } from '../types/types';
 
 interface NavbarProps {
   user: User | null;
