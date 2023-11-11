ef858d22d71252b9886e88f2ed4d0ce483d174e3
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-11 18:12:10 +0100

    feat: add Jest code coverage configuration in frontend

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -10,5 +10,21 @@ module.exports = {
       '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+    collectCoverage: true,
+    collectCoverageFrom: [
+      "src/**/*.{js,jsx,ts,tsx}",
+      "!**/node_modules/**",
+      "!**/vendor/**"
+    ],
+    coverageDirectory: "<rootDir>/coverage",
+    coverageReporters: ["lcov", "text"],
+    coverageThreshold: {
+      global: {
+        branches: 50,
+        functions: 50,
+        lines: 50,
+        statements: 50
+      }
+    }
   };
   
