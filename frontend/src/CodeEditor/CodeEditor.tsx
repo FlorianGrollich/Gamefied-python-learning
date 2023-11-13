a25f14f6184b48e4b5e0964ba1a25a486a43b8cf
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-13 21:20:38 +0100

    feat: add built-in and custom autocomplete suggestions to CodeEditor

--- frontend/src/CodeEditor/CodeEditor.tsx
+++ frontend/src/CodeEditor/CodeEditor.tsx
@@ -1,6 +1,38 @@
-import { Editor } from '@monaco-editor/react'
+import React, { useEffect } from 'react';
+import { Editor } from '@monaco-editor/react';
+import * as monaco from 'monaco-editor';
 
-const CodeEditor = () => {
+const CodeEditor: React.FC = () => {
+  const getAutocompleteSuggestions = (
+    model: monaco.editor.ITextModel,
+    position: monaco.Position
+  ) => {
+    const word = model.getWordUntilPosition(position);
+    const range = new monaco.Range(
+      position.lineNumber,
+      word.startColumn,
+      position.lineNumber,
+      word.endColumn
+    );
+
+    // This section makes custom suggestions for the autocomplete.
+    const suggestions: monaco.languages.CompletionItem[] = [
+
+    ];
+
+    return { suggestions: suggestions, incomplete: false };
+  };
+
+  useEffect(() => {
+    const provider = monaco.languages.registerCompletionItemProvider('python', {
+      provideCompletionItems: getAutocompleteSuggestions,
+    });
+
+    return () => {
+      provider.dispose();
+    };
+  }, []);
+
   return (
     <div className="p-8 m-8 bg-slate-950 rounded-lg w-3/6">
       <Editor
@@ -11,6 +43,6 @@ const CodeEditor = () => {
       />
     </div>
   )
-}
+};
 
-export default CodeEditor
+export default CodeEditor;
