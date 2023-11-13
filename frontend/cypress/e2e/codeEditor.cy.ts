224d5b55de24ebf13d91478c9db89c2a23ee5942
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-13 20:17:34 +0100

    feat: add test to codeEditor.cy.ts checking the autocomplete suggestion function

--- frontend/cypress/e2e/codeEditor.cy.ts
+++ frontend/cypress/e2e/codeEditor.cy.ts
@@ -3,5 +3,11 @@ describe('Code Editor', () => {
     cy.visit('http://localhost:3000/editor');
     cy.get('.monaco-editor').type('import ', { delay: 100 });
     cy.get('.monaco-editor').should('contain', 'import');
+    cy.get('.monaco-editor').type('{enter}', { delay: 100 });
+    cy.get('.monaco-editor').type('im', { delay: 100 });
+    cy.wait(500);
+    cy.get('.suggest-widget').should('be.visible');
+    cy.get('.suggest-widget .monaco-list-row').contains('import').click();
+    cy.get('.monaco-editor').should('contain.text', 'import');
   });
 });
