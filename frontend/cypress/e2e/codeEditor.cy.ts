70bb9bf262b987f4e3bceab1da36d958137ef097
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-13 20:14:35 +0100

    feat: add writing test to CodeEditor component

--- frontend/cypress/e2e/codeEditor.cy.ts
+++ frontend/cypress/e2e/codeEditor.cy.ts
@@ -1,5 +1,7 @@
 describe('Code Editor', () => {
   it('should display autocomplete suggestions', () => {
     cy.visit('http://localhost:3000/editor');
+    cy.get('.monaco-editor').type('import ', { delay: 100 });
+    cy.get('.monaco-editor').should('contain', 'import');
   });
 });
