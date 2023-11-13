ff8e77106e93dcefcdfcf26815cb7c6b92da8dc7
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-13 20:23:01 +0100

    chore: add last empty line to codeEditor.cy.ts

--- frontend/cypress/e2e/codeEditor.cy.ts
+++ frontend/cypress/e2e/codeEditor.cy.ts
@@ -20,16 +20,4 @@ describe('Code Editor', () => {
     cy.get('.suggest-widget .monaco-list-row').contains('import').click();
     cy.get('.monaco-editor').should('contain.text', 'import');
   });
-
-  it('should display autocomplete suggestions', () => {
-    cy.get('.monaco-editor').type('{enter}', { delay: 100 });
-    cy.get('.monaco-editor').type('import ', { delay: 100 });
-    cy.get('.monaco-editor').should('contain', 'import');
-    cy.get('.monaco-editor').type('{enter}', { delay: 100 });
-    cy.get('.monaco-editor').type('im', { delay: 100 });
-    cy.wait(500);
-    cy.get('.suggest-widget').should('be.visible');
-    cy.get('.suggest-widget .monaco-list-row').contains('import').click();
-    cy.get('.monaco-editor').should('contain.text', 'import');
-  });
 });
