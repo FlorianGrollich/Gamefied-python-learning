041d0da7031367abdc5b3e67d896903e3ad12dfe
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-13 22:39:27 +0100

    refactor: extract getAutocompleteSuggestions from CodeEditor

--- frontend/src/CodeEditor/CodeEditor.tsx
+++ frontend/src/CodeEditor/CodeEditor.tsx
@@ -23,6 +23,24 @@ const getAutocompleteSuggestions = (
   return { suggestions: suggestions, incomplete: false }
 }
 
+const getAutocompleteSuggestions = (
+  model: monaco.editor.ITextModel,
+  position: monaco.Position,
+) => {
+  const word = model.getWordUntilPosition(position)
+  const range = new monaco.Range(
+    position.lineNumber,
+    word.startColumn,
+    position.lineNumber,
+    word.endColumn,
+  )
+
+  // This section makes custom suggestions for the autocomplete.
+  const suggestions: monaco.languages.CompletionItem[] = []
+
+  return { suggestions: suggestions, incomplete: false }
+}
+
 const CodeEditor: React.FC = () => {
 
   const navigate = useNavigate();
