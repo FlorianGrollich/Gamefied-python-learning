11aa081dff26547acb0ee62480835b8d9c83013f
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-13 13:49:12 +0100

    chore: run lint-format-all

--- frontend/src/NavBar/NavBar.tsx
+++ frontend/src/NavBar/NavBar.tsx
@@ -3,8 +3,8 @@ import { Link, NavLink, useLocation } from 'react-router-dom'
 import { User } from '../types/types'
 
 interface NavBarProps {
-  user: User | null;
-  onLogout: () => void;
+  user: User | null
+  onLogout: () => void
 }
 
 const NavBar: React.FC<NavBarProps> = ({ user, onLogout }) => {
@@ -13,8 +13,8 @@ const NavBar: React.FC<NavBarProps> = ({ user, onLogout }) => {
   const dropdownRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    setIsDropdownOpen(false);
-  }, [location]);
+    setIsDropdownOpen(false)
+  }, [location])
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -40,13 +40,19 @@ const NavBar: React.FC<NavBarProps> = ({ user, onLogout }) => {
   return (
     <nav className="bg-slate-800 py-3 fixed top-0 w-full z-30 shadow">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold text-white hover:text-blue-400 transition duration-300">
+        <Link
+          to="/"
+          className="text-2xl font-bold text-white hover:text-blue-400 transition duration-300"
+        >
           Python Playground
         </Link>
         <div className="flex items-center">
           {user ? (
             <>
-              <NavLink to="/editor" className="text-white px-3 py-1 mx-2 hover:text-blue-400 transition duration-300 rounded-md">
+              <NavLink
+                to="/editor"
+                className="text-white px-3 py-1 mx-2 hover:text-blue-400 transition duration-300 rounded-md"
+              >
                 Editor
               </NavLink>
               <div
@@ -63,10 +69,16 @@ const NavBar: React.FC<NavBarProps> = ({ user, onLogout }) => {
                 </div>
                 {isDropdownOpen && (
                   <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                    <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">
+                    <Link
+                      to="/profile"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white"
+                    >
                       Profile
                     </Link>
-                    <button onClick={onLogout} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">
+                    <button
+                      onClick={onLogout}
+                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white"
+                    >
                       Logout
                     </button>
                   </div>
@@ -75,10 +87,16 @@ const NavBar: React.FC<NavBarProps> = ({ user, onLogout }) => {
             </>
           ) : (
             <>
-              <NavLink to="/login" className="text-white px-3 py-1 mx-2 hover:text-blue-400 transition duration-300 rounded-md">
+              <NavLink
+                to="/login"
+                className="text-white px-3 py-1 mx-2 hover:text-blue-400 transition duration-300 rounded-md"
+              >
                 Login
               </NavLink>
-              <NavLink to="/register" className="text-white px-3 py-1 mx-2 hover:text-blue-400 transition duration-300 rounded-md">
+              <NavLink
+                to="/register"
+                className="text-white px-3 py-1 mx-2 hover:text-blue-400 transition duration-300 rounded-md"
+              >
                 Register
               </NavLink>
             </>
@@ -86,7 +104,7 @@ const NavBar: React.FC<NavBarProps> = ({ user, onLogout }) => {
         </div>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
