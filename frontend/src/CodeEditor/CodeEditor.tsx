2601ab73f19054b2a13d3bc9fd26683209b28afc
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-13 20:42:01 +0100

    chore: fix type in CodeEditor.tsx theme attribute

--- frontend/src/CodeEditor/CodeEditor.tsx
+++ frontend/src/CodeEditor/CodeEditor.tsx
@@ -4,7 +4,7 @@ const CodeEditor = () => {
   return (
     <div className="p-8 m-8 bg-slate-950 rounded-lg w-3/6">
       <Editor
-        theme="vs-darkd"
+        theme="vs-dark"
         height="90vh"
         defaultLanguage="python"
         defaultValue="#Type your code here"
