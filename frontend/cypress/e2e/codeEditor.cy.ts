e304a576da1ab5d8f4625d440854f381c23c54c3
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-13 20:20:46 +0100

    refactor: add typing 'enter' to make test run successfully

--- frontend/cypress/e2e/codeEditor.cy.ts
+++ frontend/cypress/e2e/codeEditor.cy.ts
@@ -1,6 +1,7 @@
 describe('Code Editor', () => {
   it('should display autocomplete suggestions', () => {
     cy.visit('http://localhost:3000/editor');
+    cy.get('.monaco-editor').type('{enter}', { delay: 100 });
     cy.get('.monaco-editor').type('import ', { delay: 100 });
     cy.get('.monaco-editor').should('contain', 'import');
     cy.get('.monaco-editor').type('{enter}', { delay: 100 });
