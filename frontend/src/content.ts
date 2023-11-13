b329a42f591461f78877dfe30a089e7c8caaae66
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-13 03:28:46 +0100

    feat: move welcome section titles and descriptions into content file

---
+++ frontend/src/content.ts
@@ -0,0 +1,11 @@
+export const content = {
+    welcomeTitle: "Discover Coding Adventures with Python Playground",
+    welcomeDescription: "Embark on a thrilling coding journey with Python Playground...",
+    featureLeftTitle: "Experience Interactive Coding Riddles",
+    featureLeftDescription: "Delve into captivating riddles and grasp Python fundamentals in a delightful, interactive manner.",
+    featureRightTitle: "Join a Vibrant Learning Community",
+    featureRightDescription: "Connect with peers, celebrate your progress, and receive valuable feedback to enhance your coding skills.",
+    featureThirdTitle: "Create Your Own Coding Challenges",
+    featureThirdDescription: "Harness our integrated Python interpreter and instant output display to ignite your creativity. Watch your code come to life in real-time as you mold it."
+  };
+  
