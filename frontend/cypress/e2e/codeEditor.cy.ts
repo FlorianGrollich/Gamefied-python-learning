cb7a085026cf2b4724a73ab3aa706d839d798fd4
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-14 16:38:47 +0100

    fix: simulate logged-in user for authorisation to use the /editor route

--- frontend/cypress/e2e/codeEditor.cy.ts
+++ frontend/cypress/e2e/codeEditor.cy.ts
@@ -1,6 +1,18 @@
 describe('Code Editor', () => {
-  it('should display autocomplete suggestions', () => {
+  beforeEach(() => {
+    // Mock user data
+    const user = {
+      id: 1,
+      email: 'test@example.com',
+      username: 'testuser'
+    };
+
+    // Set user data in local storage to simulate a logged-in user
+    localStorage.setItem('user', JSON.stringify(user));
     cy.visit('http://localhost:3000/editor');
+  });
+
+  it('should display autocomplete suggestions', () => {
     cy.get('.monaco-editor').type('{enter}', { delay: 100 });
     cy.get('.monaco-editor').type('import ', { delay: 100 });
     cy.get('.monaco-editor').should('contain', 'import');
