151c2fcc0ae7cc8d981a565bc09b73d8406d000c
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2023-11-15 01:29:07 +0100

    refactor: rename error parameter to fetchError to differentiate from error state variable

--- frontend/src/GameGrid/hooks/useGameGrid.ts
+++ frontend/src/GameGrid/hooks/useGameGrid.ts
@@ -1,33 +1,33 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-type GridDataType = string[][]
+type GridDataType = string[][];
 
 const useFetchGrid = (url: string) => {
-  const [gridData, setGridData] = useState<GridDataType | null>(null)
-  const [loading, setLoading] = useState<boolean>(true)
-  const [error, setError] = useState<Error | null>(null)
+  const [gridData, setGridData] = useState<GridDataType | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     fetch(url)
       .then(response => {
         if (!response.ok) {
-          console.log(response)
-          throw new Error('Network response was not ok')
+          console.log(response);
+          throw new Error('Network response was not ok');
         }
-        return response.json()
+        return response.json();
       })
       .then(data => {
-        console.log(data)
-        setGridData(JSON.parse(data))
-        setLoading(false)
+        console.log(data);
+        setGridData(JSON.parse(data));
+        setLoading(false);
       })
-      .catch((error: Error) => {
-        setError(error)
-        setLoading(false)
-      })
-  }, [url])
+      .catch((fetchError: Error) => {
+        setError(fetchError);
+        setLoading(false);
+      });
+  }, [url]);
 
-  return { gridData, loading, error }
-}
+  return { gridData, loading, error };
+};
 
-export default useFetchGrid
+export default useFetchGrid;
