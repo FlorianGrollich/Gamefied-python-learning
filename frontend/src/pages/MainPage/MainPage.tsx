f1c2e1a996782c0005e5633e385f9341ebef5169
FlorianGrollich <F-Grollich@web.de>
2024-02-24 13:55:11 +0100

    feat: card around editor

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-
+import CodeEditor from "./components/CodeEditor";
 
 const MainPage: React.FC = () => {
-
     return (
-        <div></div>
+        <div className="grid grid-cols-2">
+            <div className="col-start-1 col-end-2 p-4">
+                    <CodeEditor />
+            </div>
+            <div>
+
+            </div>
+        </div>
     )
 }
 
