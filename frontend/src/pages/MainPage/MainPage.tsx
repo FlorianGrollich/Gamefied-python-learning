c437799809851b3f87b9928bfa7f282f68b833ca
FlorianGrollich <F-Grollich@web.de>
2024-04-06 18:36:06 +0200

    feat: create ws connection with backend on port 8080

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 import CodeEditor from "./components/CodeEditor";
 import GameGrid from "./components/GameGrid";
-
 const MainPage: React.FC = () => {
+
+    useEffect(() => {
+        const socket = new WebSocket('ws://localhost:8080');
+
+        socket.addEventListener('open', function (event) {
+            socket.send('Hello from frontend!');
+        });
+
+        socket.addEventListener('message', function (event) {
+            console.log('Message from server: ', event.data);
+        });
+
+        // Clean up the effect
+        return () => socket.close();
+    }, []);
+
+
     return (
         <div className="grid grid-cols-2">
             <div className="col-start-1 col-end-2 p-4">
