b4b81a1ab98e0ff526a63f9721758079108c55f3
FlorianGrollich <F-Grollich@web.de>
2024-04-12 14:45:00 +0200

    feat: add player action and player class

---
+++ gameengine/player/player.py
@@ -0,0 +1,13 @@
+from player_actions import PlayerActions
+
+
+
+class Player:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.backpack = []
+
+
+
+
---
+++ gameengine/player/player_actions.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class PlayerActions(Enum):
+    Move = "Move"
+    Turn = "Turn"
+    Inspect = "Inspect"
+    Take = "Check"
