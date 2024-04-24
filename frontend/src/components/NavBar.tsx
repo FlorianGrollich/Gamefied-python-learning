312da8948b116e5ea4b82f6037f7708748c19a66
FlorianGrollich <F-Grollich@web.de>
2024-04-24 13:56:21 +0200

    refactor: remove unused imports

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect, useRef} from 'react'
-import {Link, NavLink, useLocation} from 'react-router-dom'
+import React from 'react'
+import {Link} from 'react-router-dom'
 
 
 interface NavBarItemProps {
