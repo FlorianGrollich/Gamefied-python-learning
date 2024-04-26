7091f8675ffd8b04722cbcc2b6e24e834b45e4cb
FlorianGrollich <F-Grollich@web.de>
2024-04-26 10:30:04 +0200

    feat: add PlayerActions enum

--- frontend/src/pages/MainPage/components/GameGrid.tsx
+++ frontend/src/pages/MainPage/components/GameGrid.tsx
@@ -29,7 +29,7 @@ const GameGrid: React.FC<GameGridProps> = ({ cubePositions }) => {
           position={cubePositions}
         />
 
-        <mesh position={[0, 0, 0]}>
+        <mesh position={[0, -5, 0]}>
           <boxGeometry args={[400, 10, 400]} />
           <meshBasicMaterial>
             <GradientTexture stops={[0, 1]} colors={['#ff9c00', '#ffdd33']} />
