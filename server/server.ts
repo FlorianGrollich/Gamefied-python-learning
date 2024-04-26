837aabc5da37051df7fdf65d5f14345b04908803
FlorianGrollich <F-Grollich@web.de>
2024-04-26 06:17:24 +0200

    fix: put server into websocket

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 import express, {Express, Request, Response, NextFunction} from 'express'
@@ -19,7 +20,6 @@ const app: Express = express()
 PostgresDataSource.initialize().then(() => {
 
 
-
     if (!process.env.JWT_SECRET) {
         console.error('JWT_SECRET is not set')
     }
@@ -32,48 +32,45 @@ PostgresDataSource.initialize().then(() => {
         console.log(`Server listening on port: ${PORT}`);
     });
 
+    const wss = new WebSocket.Server(server);
 
+    wss.on('connection', ws => {
+        ws.on('message', message => {
+            console.log('received: %s', message);
+            let options = {
+                mode: 'text',
+                pythonOptions: ['-c'], // Execute code as a command line argument
+            };
+
+            writeFile('gameengine/player/main.py', message.toString(), (err) => {
+                if (err) {
+                    console.error('Failed to write to main.py:', err);
+                    return;
+                }
+                console.log("Run code")
+                let options: Options = {
+                    mode: 'text',
+                    pythonOptions: ['-u'],
+                    scriptPath: '../gameengine/player/',
+                    pythonPath: 'C:\\Users\\F-Gro\\AppData\\Local\\Programs\\Python\\Python312\\python.exe'
+                };
+                // Execute the modified script
+                PythonShell.run("main.py", options).then((output) => {
+                    console.log(output);
+                    ws.send(output.toString());
+                }).catch(err => {
+                    console.error('Failed to run Python script:', err);
+                });
+            });
+
+        });
+
+        ws.send('Hello from backend!');
+    });
 
 }).catch(error => {
     console.error('Error during Data Source initialization. \n 1.Does docker run?\n Error:', error);
 });
 
-
-
-const wss = new WebSocket.Server( );
-
-wss.on('connection', ws => {
-    ws.on('message', message => {
-        console.log('received: %s', message);
-        let options = {
-            mode: 'text',
-            pythonOptions: ['-c'], // Execute code as a command line argument
-        };
-
-        writeFile('gameengine/player/main.py', message.toString(), (err) => {
-            if (err) {
-                console.error('Failed to write to main.py:', err);
-                return;
-            }
-            console.log("Run code")
-            let options: Options = {
-                mode: 'text',
-                pythonOptions: ['-u'],
-                scriptPath: '../gameengine/player/',
-                pythonPath: 'C:\\Users\\F-Gro\\AppData\\Local\\Programs\\Python\\Python312\\python.exe'
-            };
-            // Execute the modified script
-            PythonShell.run("main.py", options).then((output) => {
-                console.log(output);
-                ws.send(output.toString());
-            }).catch(err => {
-                console.error('Failed to run Python script:', err);
-            });
-        });
-
-    });
-
-    ws.send('Hello from backend!');
-});
 
 export default app;
