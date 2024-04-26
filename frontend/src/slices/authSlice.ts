185079094b203bda9fda46d5bc8038e8d6e0f5a4
FlorianGrollich <F-Grollich@web.de>
2024-04-26 06:04:35 +0200

    fix- cors

--- frontend/src/slices/authSlice.ts
+++ frontend/src/slices/authSlice.ts
@@ -17,7 +17,7 @@ const initialState: AuthState = {
 export const register = createAsyncThunk(
     'auth/register',
     ({username, email, password}: { username: string; password: string, email: string }, {rejectWithValue}) => {
-        return axios.post('https://banana-backend-hcsacywj4-flos-projects-7df6fe2d.vercel.app/api/register', {username, email, password})
+        return axios.post('https://tranquil-wildwood-44813-824cd9c16627.herokuapp.com/api/register', {username, email, password})
             .then(response => {
                 return response.data.token;
             })
@@ -32,7 +32,7 @@ export const register = createAsyncThunk(
 export const login = createAsyncThunk(
     'auth/login',
     ({username, password}: { username: string; password: string }, {rejectWithValue}) => {
-        return axios.post('https://banana-backend-hcsacywj4-flos-projects-7df6fe2d.vercel.app/api/login', {username, password})
+        return axios.post('https://tranquil-wildwood-44813-824cd9c16627.herokuapp.com/api/login', {username, password})
             .then(response => {
                 return response.data.token;
             })
--- server/middleware/index.ts
+++ server/middleware/index.ts
@@ -6,15 +6,9 @@ import { Express } from 'express';
 
 
 export function setupMiddleware(app: Express) {
-    const corsOptions = {
-        origin: 'https://banana-frontend-jecx.vercel.app', // Adjust this to the domain from which you expect to get requests
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-        credentials: true, // If your front-end needs to send cookies or credentials
-        optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-    };
 
-    app.use(cors(corsOptions));
+
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(rateLimiter());
 }
