cd5970b0aa150533896a951ee8fd5f1d94d9f859
FlorianGrollich <F-Grollich@web.de>
2024-05-14 19:34:53 +0200

    feat: add tests for login page

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom', // change this line
+  transformIgnorePatterns: [
+    "/node_modules/(?!axios).+\\.js$"
+  ],
+
+  setupFilesAfterEnv: ['./jest.setup.ts'],
 };
+export {};
