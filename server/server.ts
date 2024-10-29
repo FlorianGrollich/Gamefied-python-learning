5fed4dbfae9a4246cfbbed12c999caa251cec7f3
FlorianGrollich <F-Grollich@web.de>
2024-10-29 17:44:34 +0100

    feat: add mongodb connection

--- server/server.ts
+++ server/server.ts
@@ -2,75 +2,71 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import express, {Express, Request, Response, NextFunction} from 'express'
-import {createServer} from 'http'
-import {PostgresDataSource} from './utils/data-source'
-import routes from "./routes";
-import {PORT} from "./config";
-import {setupMiddleware} from "./middleware";
+import express, { Express, Request, Response, NextFunction } from 'express';
+import { createServer } from 'http';
+import { PostgresDataSource } from './utils/data-source';
+import routes from './routes';
+import { PORT } from './config';
+import { setupMiddleware } from './middleware';
 
-import WebSocket from "ws";
+import WebSocket from 'ws';
 import swaggerUi from 'swagger-ui-express';
-import {PythonShell, Options} from "python-shell";
-import {writeFile} from "node:fs";
+import { PythonShell, Options } from 'python-shell';
+import { writeFile } from 'node:fs';
+import * as mongoose from 'mongoose';
+import { connectMongo } from './config/mongo';
 
-const app: Express = express()
+const app: Express = express();
 
 
-PostgresDataSource.initialize().then(() => {
+if (!process.env.JWT_SECRET) {
+  console.error('JWT_SECRET is not set');
+}
 
+connectMongo().catch(err => {console.error(err)});
 
-    if (!process.env.JWT_SECRET) {
-        console.error('JWT_SECRET is not set')
-    }
+setupMiddleware(app);
+app.use('/api', routes);
+const server = createServer(app);
 
-    setupMiddleware(app)
-    app.use('/api', routes)
-    const server = createServer(app);
-
-    server.listen(PORT, () => {
-        console.log(`Server listening on port: ${PORT}`);
-    });
-
-    const wss = new WebSocket.Server({server});
-
-    wss.on('connection', ws => {
-        ws.on('message', message => {
-            console.log('received: %s', message);
-            let options = {
-                mode: 'text',
-                pythonOptions: ['-c'], // Execute code as a command line argument
-            };
-
-            writeFile('gameengine/player/main.py', message.toString(), (err) => {
-                if (err) {
-                    console.error('Failed to write to main.py:', err);
-                    return;
-                }
-                console.log("Run code")
-                let options: Options = {
-                    mode: 'text',
-                    pythonOptions: ['-u'],
-                    scriptPath: 'gameengine/player/',
-                    pythonPath: 'python'
-                };
-                // Execute the modified script
-                PythonShell.run("main.py", options).then((output) => {
-                    console.log(output);
-                    ws.send(output.toString());
-                }).catch(err => {
-                    console.error('Failed to run Python script:', err);
-                });
-            });
-
-        });
-
-        ws.send('Hello from backend!');
-    });
-
-}).catch(error => {
-    console.error('Error during Data Source initialization. \n 1.Does docker run?\n Error:', error);
+server.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
 });
 
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log('received: %s', message);
+    let options = {
+      mode: 'text',
+      pythonOptions: ['-c'], // Execute code as a command line argument
+    };
+
+    writeFile('gameengine/player/main.py', message.toString(), (err) => {
+      if (err) {
+        console.error('Failed to write to main.py:', err);
+        return;
+      }
+      console.log('Run code');
+      let options: Options = {
+        mode: 'text',
+        pythonOptions: ['-u'],
+        scriptPath: 'gameengine/player/',
+        pythonPath: 'python',
+      };
+      // Execute the modified script
+      PythonShell.run('main.py', options).then((output) => {
+        console.log(output);
+        ws.send(output.toString());
+      }).catch(err => {
+        console.error('Failed to run Python script:', err);
+      });
+    });
+
+  });
+
+  ws.send('Hello from backend!');
+});
 
 export default app;
