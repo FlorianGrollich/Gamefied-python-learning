459e1abef792a4e238e25b98a5556e6b65d40394
FlorianGrollich <F-Grollich@web.de>
2024-11-13 17:26:30 +0100

    feat: add createSession on SessionController

--- server/controller/SessionController.ts
+++ server/controller/SessionController.ts
@@ -1,14 +1,33 @@
-import { NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { Session } from '../models/sessionModel';
+import jwt from 'jsonwebtoken';
+
+
 
 
 class SessionController {
 
   constructor() {
-
+    this.createSession = this.createSession.bind(this);
   }
 
 
   async createSession(req: Request, res: Response, next: NextFunction) {
-    
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).send('Unauthorized');
+    }
+
+    //get token from authHeader
+    const token = authHeader.split(' ')[1];
+    try {
+      const { email } = jwt.verify(token, process.env.JWT_SECRET!) as { email: string };
+      const session = new Session({ usersIds: [email], code: '1234' });
+      await session.save();
+      res.status(201).json(session);
+    } catch (err) {
+        res.status(401).send('Unauthorized');
+    }
   }
 }
