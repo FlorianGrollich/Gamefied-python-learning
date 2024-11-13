0aec83debe53953e33206c49a6cb87967c8b83cd
FlorianGrollich <F-Grollich@web.de>
2024-11-13 15:55:21 +0100

    feat: add session controller

--- frontend/src/pages/Menu/MenuPage.tsx
+++ frontend/src/pages/Menu/MenuPage.tsx
@@ -4,8 +4,13 @@ import React from 'react';
 const MenuPage : React.FC = () => {
 
   return (
-    <div>
-      <h1>Test</h1>
+    <div className={"grid grid-cols-9 grid-rows-9 gap-4 min-h-screen"}>
+      <div className={"col-start-1"}></div>
+      <div className={"bg-white col-start-2 col-end-6 row-start-2 row-end-9"}>
+        Create New Game
+      </div>
+      <div className={"col-start-6 col-end-9 row-start-2 row-end-5 bg-red-600"}></div>
+      <div className={"col-start-6 "}></div>
     </div>
   );
 }
