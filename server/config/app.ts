dd2eb12b6f6eb8e125bba2c89f66fe6cdf781e09
FlorianGrollich <F-Grollich@web.de>
2024-11-14 23:48:43 +0100

    feat: add sentry express error handler

--- server/config/app.ts
+++ server/config/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import expressWs from 'express-ws';
 import routes from '../routes';
 import { setupMiddleware } from '../middleware';
+import * as Sentry from '@sentry/node';
 
 const app: Express = express();
 expressWs(app);
@@ -9,6 +10,7 @@ expressWs(app);
 setupMiddleware(app);
 
 app.use('/api', routes);
+Sentry.setupExpressErrorHandler(app);
 
 
 export default app;
