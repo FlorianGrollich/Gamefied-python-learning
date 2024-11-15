a9243f6b80f3806d5daa73f67e1ec9f0b8f91980
FlorianGrollich <F-Grollich@web.de>
2024-11-15 10:08:17 +0100

    fix

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,8 +1,40 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = function override(config, env) {
-    config.plugins.push(new MonacoWebpackPlugin({
-        languages: ['json', 'python']
-    }));
+    config.plugins.push(
+      new MonacoWebpackPlugin({
+          languages: ['json', 'python'],
+      }),
+      new NodePolyfillPlugin()
+    );
+
+    config.resolve = {
+        ...config.resolve,
+        alias: {
+            // Alias to remove `node:` protocol issues
+            'node:fs': 'fs',
+            'node:tls': 'tls',
+            'node:net': 'net',
+            'node:path': 'path',
+            'node:zlib': 'zlib',
+            'node:http': 'http',
+            'node:https': 'https',
+            'node:stream': 'stream',
+            'node:crypto': 'crypto',
+        },
+        fallback: {
+            fs: false,
+            tls: false,
+            net: false,
+            path: require.resolve('path-browserify'),
+            zlib: require.resolve('browserify-zlib'),
+            http: require.resolve('stream-http'),
+            https: require.resolve('https-browserify'),
+            stream: require.resolve('stream-browserify'),
+            crypto: require.resolve('crypto-browserify'),
+        },
+    };
+
     return config;
-}
+};
