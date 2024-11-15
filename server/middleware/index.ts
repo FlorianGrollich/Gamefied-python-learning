4b8103de15d9e468537030586f8108948c3188de
FlorianGrollich <F-Grollich@web.de>
2024-11-15 10:39:03 +0100

    fix cors

--- server/middleware/index.ts
+++ server/middleware/index.ts
@@ -4,7 +4,7 @@ import { rateLimiter } from './rateLimiter';
 import cors from 'cors';
 
 export function setupMiddleware(app: Express) {
-  app.use(cors({ origin: 'https://banana-frontend-jecx-jpzia7buw-flos-projects-7df6fe2d.vercel.app' }));
+  app.use(cors({ origin: ['https://banana-frontend-jecx-jpzia7buw-flos-projects-7df6fe2d.vercel.app'] }));
   app.use(bodyParser.json());
   app.use(rateLimiter());
 
